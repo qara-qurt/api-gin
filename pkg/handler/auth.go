811e9da8806981795265a93553de0f6893bae932
Dias <serikov.2002.12@gmail.com>
2022-12-15 20:20:35 +0600

    Added token in SignIn

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -26,6 +26,26 @@ func (h *Handler) SignUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) SignIn(c *gin.Context) {
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func (h *Handler) SignIn(c *gin.Context) {
+	var input signInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
